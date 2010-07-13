57bd83f69c0f8402a11e325ccbf36c83c8acc800
Daniel Jackoway <danjdel@gmail.com>
2010-07-13 11:10:21 +0800

    add tests for #verify_webhook_signature

--- test/notifo_test.rb
+++ test/notifo_test.rb
@@ -4,4 +4,15 @@ class NotifoTest < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
+
+  context "#verify_webhook_sigature" do
+    should "return true if given the right API secret" do
+      assert NOTIFO.verify_webhook_signature(WEBHOOK)
+    end
+
+    should "return false if given the wrong API secret" do
+      bad_notifo = Notifo.new NOTIFO_USERNAME, 'thisisntthesecret'
+      assert !bad_notifo.verify_webhook_signature(WEBHOOK)
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,10 @@ require 'notifo'
 
 class Test::Unit::TestCase
 end
+
+NOTIFO_USERNAME = 'testing42'
+NOTIFO_API_SECRET = 'x96cb52807a33bafc8fd741eeba5e40ff89f05896'
+
+NOTIFO = Notifo.new NOTIFO_USERNAME, NOTIFO_API_SECRET
+
+WEBHOOK = YAML::load(File.read('test/assets/webhook_params.yaml'))
