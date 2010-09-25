f5ebbf7453f2342d2e567210d944c1883b3a99f7
phsr <phasor@phsr.org>
2010-09-25 15:32:33 -0400

    Removed default shoulda test.  Added tests for Notifo#subscribe_user and Notifo#post

--- test/notifo_test.rb
+++ test/notifo_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class NotifoTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  
+  should 'Notifo#subscribe_user should return a hash' do
+    assert NOTIFO.subscribe_user(NOTIFO_USERNAME).kind_of? Hash
   end
-
+  
+  should 'Notifo#post should return a hash' do
+    assert NOTIFO.post(NOTIFO_USERNAME, "message").kind_of? Hash
+  end
+  
   context "#verify_webhook_sigature" do
     should "return true if given the right API secret" do
       assert NOTIFO.verify_webhook_signature(WEBHOOK)
