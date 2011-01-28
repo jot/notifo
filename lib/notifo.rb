167f50a362bd3513a0ccdf6ea29eae34e7da6b5c
Sam Mulube <sam@connectedenvironments.com>
2011-01-28 21:20:58 +0000

    Slight revision to main notifo interface.
    * Renamed `post` method to `send_notification`. This matches the HTTP API
    method and is I think a bit clearer particularly now that we've added
    the ability to send messages as well as notifications.
    * Kept the old `post` method, but have added a deprecation warning. This
    should mean we don't break any code using the old method.
    * Added `send_message` method which correlates to the corresponding
    method in the HTTP API.
    * Cleaned up a couple of typos and tidied source (pedantic white space
    clean up).

--- lib/notifo.rb
+++ lib/notifo.rb
@@ -2,37 +2,53 @@ require 'httparty'
 require 'cgi'
 require 'digest/sha1'
 
+# Simple client class for the Notifo API.
 class Notifo
   include HTTParty
   base_uri 'https://api.notifo.com/v1'
 
-  # Required Parameters
+  # Create a new client instance. Must be supplied with the username and
+  # api_secret for your service.
+  #
+  # Required Parameters:
   # username - notifo service username
-  # apikey - notifo service apisecret
-  def initialize(username, apisecret)
-    @auth = {:username => username, :password => apisecret}
+  # api_secret - notifo service api secret
+  def initialize(username, api_secret)
+    @auth = { :username => username, :password => api_secret }
   end
   
-  # Required Parameters
+  # Subscribe a user to your service. This sends them a message confirming they 
+  # want to use the service.
+  #
+  # Required Parameters:
   # username - notifo username to subscribe to your service
   def subscribe_user(username)
-    options = { :body => {:username => username}, :basic_auth => @auth }
+    options = { :body => { :username => username }, :basic_auth => @auth }
     self.class.post("/subscribe_user", options)
   end
   
-  # Required Parameters
-  # to - notifo username of recipient
-  # msg - message being sent; must be url encoded
-  # Optional Parameters
+  # Send a notification to the specified user. You will only be allowed to send
+  # notifications to users who have previously subscribed to your service.
+  #
+  # Required Parameters:
+  # username - notifo username of recipient
+  # message - message being sent; must be url encoded
+  #
+  # Optional Parameters:
   # title - name of "notification event"
   # uri - the uri that will be loaded when the notification is opened; if specified, must be urlencoded; if a web address, must start with http:// or https://
   # label - label describing the "application" (used only if being sent from a User account; the Service label is automatically applied if being sent from a Service account) 
-  def post(username, msg, title=nil, uri=nil, label=nil)
-    options = { :body => {:to => username, :msg => msg, :label=>title, :title=>title, :uri=>uri}, :basic_auth => @auth }
+  def send_notification(username, message, title = nil, uri = nil, label = nil)
+    options = { :body => {:to => username, :msg => message, :label => title, :title => title, :uri => uri}, :basic_auth => @auth }
     self.class.post('/send_notification', options)
   end
 
-  # Require Parameters
+  def post(username, message, title = nil, uri = nil, label = nil) # :ndoc:
+    warn "DEPRECATION WARNING: the 'post' method is deprecated. Use 'send_notification' instead"
+    send_notification(username, message, title, uri, label)
+  end
+
+  # Required Parameters:
   # params - the hash of params the webhook passed to you. All keys must be *Strings*, not symbols.
   def verify_webhook_signature(params)
     signature = params['notifo_signature']
@@ -43,5 +59,17 @@ class Notifo
     str << @auth[:password]
 
     signature == Digest::SHA1.hexdigest(CGI::escape(str))
+  end
+
+  # Send a message to a specific Notifo user. Messages are slightly different
+  # from notifications in that they are permitted to be sent to users who
+  # haven't subscribed.
+  #
+  # Required Parameters:
+  # username - notifo username of recipient
+  # message - message being sent; must be url encoded
+  def send_message(username, message)
+    options = { :body => { :to => username, :msg => message }, :basic_auth => @auth }
+    self.class.post('/send_message', options)
   end
 end
